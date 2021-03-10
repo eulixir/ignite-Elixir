ca47c7001d82f10eea96d92e961e2d7d12c42884
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-10 19:01:31 -0300

    Adionar módulo que calcula a despesa de cada id

--- Módulo-2/reports_generator/lib/reports_generator.ex
+++ Módulo-2/reports_generator/lib/reports_generator.ex
@@ -1,7 +1,15 @@
 defmodule ReportsGenerator do
+
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.each(fn elem -> IO.inspect(elem) end)
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report)
+    end)
   end
+
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1) , 0})
 end
