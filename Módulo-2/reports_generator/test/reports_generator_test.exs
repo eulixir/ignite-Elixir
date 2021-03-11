5fc41e80f7924225ef5c5b559e04b18a9fecb84d
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-11 12:36:46 -0300

    Adicionar testes a aplicação

--- Módulo-2/reports_generator/test/reports_generator_test.exs
+++ Módulo-2/reports_generator/test/reports_generator_test.exs
@@ -1,8 +1,50 @@
 defmodule ReportsGeneratorTest do
   use ExUnit.Case
-  doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  @file_name "report_test.csv"
+
+  describe("build/1") do
+    test "build reports" do
+      response = report = ReportsGenerator.build(@file_name)
+
+      expected_response = report
+
+      assert response == expected_response
+    end
+  end
+
+  describe "fetch_higher_cost/2" do
+    test "when the option is 'users', returns the user who spent the most" do
+      response =
+        @file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("users")
+
+      expected_response = {:ok, {"5", 49}}
+
+      assert response == expected_response
+    end
+  end
+
+  test "when the option is 'foods', returns the most consumed food" do
+    response =
+      @file_name
+      |> ReportsGenerator.build()
+      |> ReportsGenerator.fetch_higher_cost("foods")
+
+    expected_response = {:ok, {"esfirra", 3}}
+
+    assert response == expected_response
+  end
+
+  test "when an invalid option as given, returns an error" do
+    response =
+      @file_name
+      |> ReportsGenerator.build()
+      |> ReportsGenerator.fetch_higher_cost("user")
+
+    expected_response = {:error, "Invalid option!"}
+
+    assert response == expected_response
   end
 end
