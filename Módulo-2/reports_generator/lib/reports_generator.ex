cff83d0147ba4bab4aee92d62048635a060cbf30
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-11 00:52:11 -0300

    Adicionar módulo para pesquisar a quantidade de comidas na lista

--- Módulo-2/reports_generator/lib/reports_generator.ex
+++ Módulo-2/reports_generator/lib/reports_generator.ex
@@ -2,14 +2,40 @@ defmodule ReportsGenerator do
 
   alias ReportsGenerator.Parser
 
+  @avaible_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report)
-    end)
+  end)
+end
+
+def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
+defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+
   end
 
-  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+  defp report_acc do
+    foods = Enum.into(@avaible_foods, %{}, &{(&1) , 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1) , 0})
 
-  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1) , 0})
+    %{"users" => users, "foods" => foods}
+
+  end
 end
