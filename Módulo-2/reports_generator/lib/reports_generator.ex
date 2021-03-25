68ebdd8ad9f369d61a654e2908228ab18567063a
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-25 10:35:17 -0300

    Add test

--- Módulo-2/reports_generator/lib/reports_generator.ex
+++ Módulo-2/reports_generator/lib/reports_generator.ex
@@ -20,11 +20,19 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
   def build_from_many(filenames) do
+    result =
     filenames
     |> Task.async_stream(&build/1)
     |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
   end
+
 
   defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
     "foods" => foods2,
@@ -44,7 +52,7 @@ defmodule ReportsGenerator do
    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
 
-  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
 
   defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     users = Map.put(users, id, users[id] + price)
--- Módulo-2/reports_generator/test/reports_generator_test.exs
+++ Módulo-2/reports_generator/test/reports_generator_test.exs
@@ -1,22 +1,67 @@
 defmodule ReportsGeneratorTest do
   use ExUnit.Case
 
-  @file_name "report_test.csv"
+  describe "build/1" do
+    test "builds the report" do
+      file_name = "report_test.csv"
 
-  describe("build/1") do
-    test "build reports" do
-      response = report = ReportsGenerator.build(@file_name)
+      response = ReportsGenerator.build(file_name)
 
-      expected_response = report
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pastel" => 0,
+          "pizza" => 2,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 45,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 31,
+          "30" => 0,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
 
       assert response == expected_response
     end
   end
 
   describe "fetch_higher_cost/2" do
-    test "when the option is 'users', returns the user who spent the most" do
+    test "when the option is 'users', returns the who the most spending user" do
+      file_name = "report_test.csv"
+
       response =
-        @file_name
+        file_name
         |> ReportsGenerator.build()
         |> ReportsGenerator.fetch_higher_cost("users")
 
@@ -24,27 +69,96 @@ defmodule ReportsGeneratorTest do
 
       assert response == expected_response
     end
+
+    test "when option is 'foods', returns the most eaten food" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("foods")
+
+      expected_response = {:ok, {"esfirra", 3}}
+
+      assert response == expected_response
+    end
+
+    test "when option is invalid, returns an error" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("drinks")
+
+      expected_response = {:error, "Invalid option"}
+
+      assert response == expected_response
+    end
   end
 
-  test "when the option is 'foods', returns the most consumed food" do
-    response =
-      @file_name
-      |> ReportsGenerator.build()
-      |> ReportsGenerator.fetch_higher_cost("foods")
+  describe "build_from_many/1" do
+    test "when a file list is provided, builds the report" do
+      file_names = ["report_test.csv", "report_test.csv"]
 
-    expected_response = {:ok, {"esfirra", 3}}
+      response = ReportsGenerator.build_from_many(file_names)
 
-    assert response == expected_response
-  end
+      expected_response =
+        {:ok,
+         %{
+           "foods" => %{
+             "açaí" => 2,
+             "churrasco" => 4,
+             "esfirra" => 6,
+             "hambúrguer" => 4,
+             "pastel" => 0,
+             "pizza" => 4,
+             "prato_feito" => 0,
+             "sushi" => 0
+           },
+           "users" => %{
+             "1" => 96,
+             "10" => 72,
+             "11" => 0,
+             "12" => 0,
+             "13" => 0,
+             "14" => 0,
+             "15" => 0,
+             "16" => 0,
+             "17" => 0,
+             "18" => 0,
+             "19" => 0,
+             "2" => 90,
+             "20" => 0,
+             "21" => 0,
+             "22" => 0,
+             "23" => 0,
+             "24" => 0,
+             "25" => 0,
+             "26" => 0,
+             "27" => 0,
+             "28" => 0,
+             "29" => 0,
+             "3" => 62,
+             "30" => 0,
+             "4" => 84,
+             "5" => 98,
+             "6" => 36,
+             "7" => 54,
+             "8" => 50,
+             "9" => 48
+           }
+         }}
 
-  test "when an invalid option as given, returns an error" do
-    response =
-      @file_name
-      |> ReportsGenerator.build()
-      |> ReportsGenerator.fetch_higher_cost("user")
+      assert response == expected_response
+    end
 
-    expected_response = {:error, "Invalid option!"}
+    test "when a file list is not provided, returns error" do
+      response = ReportsGenerator.build_from_many("elixir")
 
-    assert response == expected_response
+      expected_response = {:error, "Please provide a list of strings"}
+
+      assert response == expected_response
+    end
   end
 end
