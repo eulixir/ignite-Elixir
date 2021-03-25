841fcd85aadc1bff5482d420cc06d8a3a81065e8
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-25 11:54:45 -0300

    Update and create exlivery project

---
+++ Chapter-2/exlivery/lib/exlivery.ex
@@ -0,0 +1,18 @@
+defmodule Exlivery do
+  @moduledoc """
+  Documentation for `Exlivery`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exlivery.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
