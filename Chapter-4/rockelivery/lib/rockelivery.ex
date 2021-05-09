baf5b26496a8f61363dcc25b32d0e18fbde6439a
João Pedro Alves <jotalmeida007@hotmail.com>
2021-05-09 14:16:06 -0300

    Add controller and view

--- Chapter-4/rockelivery/lib/rockelivery.ex
+++ Chapter-4/rockelivery/lib/rockelivery.ex
@@ -1,4 +1,6 @@
 defmodule Rockelivery do
+  alias Rockelivery.Items.Create, as: ItemCreate
+
   alias Rockelivery.Users.Create, as: UserCreate
   alias Rockelivery.Users.Delete, as: UserDelete
   alias Rockelivery.Users.Get, as: UserGet
@@ -8,6 +10,8 @@ defmodule Rockelivery do
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
+
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
 
 # params = %{
--- Chapter-4/rockelivery/lib/rockelivery_web/views/error_view.ex
+++ Chapter-4/rockelivery/lib/rockelivery_web/views/error_view.ex
@@ -22,11 +22,18 @@ defmodule RockeliveryWeb.ErrorView do
     %{message: translate_errors(changeset)}
   end
 
+  def render("error.json", %{result: result}) do
+    %{message: result}
+  end
+
   def translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}}", to_string(value))
+        String.replace(acc, "%{#{key}}", translate_value(value))
       end)
     end)
   end
+
+  defp translate_value({:parameterized, Ecto.Enum, _map}), do: ""
+  defp translate_value(value), do: to_string(value)
 end
