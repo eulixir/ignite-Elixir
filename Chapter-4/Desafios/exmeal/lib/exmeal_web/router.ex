4e59f15879f0f726658a5101761f0db8626d3d1a
João Pedro Alves <jotalmeida007@hotmail.com>
2021-05-19 13:14:05 -0300

    Add api routes

--- Chapter-4/Desafios/exmeal/lib/exmeal_web/router.ex
+++ Chapter-4/Desafios/exmeal/lib/exmeal_web/router.ex
@@ -9,6 +9,8 @@ defmodule ExmealWeb.Router do
     pipe_through :api
 
     resources "/meals", MealsController, except: [:new, :edit]
+    resources "/users", UsersController, except: [:new, :edit]
+
   end
 
   # Enables LiveDashboard only for development
