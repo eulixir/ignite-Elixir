95ac694345e960e5c69eceddf2dfb32ee1bb1f80
João Pedro Alves <jotalmeida007@hotmail.com>
2021-05-20 00:57:09 -0300

    add client tests

--- Chapter-4/rockelivery/lib/via_cep/client.ex
+++ Chapter-4/rockelivery/lib/via_cep/client.ex
@@ -1,17 +1,33 @@
-defmodule Rockelivery.ViaCep.Cliend do
+defmodule Rockelivery.ViaCep.Client do
   use Tesla
+
+  alias Tesla.Env
+
+  alias Rockelivery.Error
 
   plug Tesla.Middleware.JSON
 
-  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws/"
+  @base_url "https://viacep.com.br/ws/"
 
-  # def get_cep_info(ceo) do
-  # "#{cep}/json/"
-  # |> get()
-  # |> handle_get()
-  # end
+  def get_cep_info(url \\ @base_url, cep) do
+    "#{url}#{cep}/json/"
+    |> get()
+    |> handle_get()
+  end
 
-  # defp handle_get({:ok, %Env{status: 200, body: %{"erro" => true}}}) do
+  defp handle_get({:ok, %Env{status: 200, body: %{"erro" => true}}}) do
+    {:error, Error.build(:not_found, "CEP not found!")}
+  end
 
-  # end
+  defp handle_get({:ok, %Env{status: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  defp handle_get({:ok, %Env{status: 400, body: _body}}) do
+    {:error, Error.build(:bad_request, "Invalid CEP")}
+  end
+
+  defp handle_get({:error, reason}) do
+    {:error, Error.build(:bad_request, reason)}
+  end
 end
