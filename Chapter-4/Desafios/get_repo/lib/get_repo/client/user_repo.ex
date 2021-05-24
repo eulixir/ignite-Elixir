69169de750992b28de049b5258b7daad0ddcf1ef
João Pedro Alves <jotalmeida007@hotmail.com>
2021-05-24 09:37:42 -0300

    Revert "Remove challenge api"
    This reverts commit b19c1b5f7f3edac2c92956a114de093191a1efbc.

---
+++ Chapter-4/Desafios/get_repo/lib/get_repo/client/user_repo.ex
@@ -0,0 +1,34 @@
+defmodule GetRepo.Client.UserRepo do
+  use Tesla
+
+  alias Tesla.Env
+  alias GetRepo.Error
+  alias GetRepo.Client.Parser
+
+  @base_url "https://api.github.com"
+  @request_headers [
+    {"User-Agent",
+     "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+  ]
+
+  plug Tesla.Middleware.Headers, @request_headers
+  plug Tesla.Middleware.JSON
+
+  def get_user_repos(url \\ @base_url, username) do
+    "#{url}/users/#{username}/repos"
+    |> get()
+    |> handle_get()
+  end
+
+  defp handle_get({:error, reason}) do
+    {:error, Error.build(:bad_request, reason)}
+  end
+
+  defp handle_get({:ok, %Env{status: 404, body: _body}}) do
+    {:error, Error.build(:not_found, "Github username not found!")}
+  end
+
+  defp handle_get({:ok, %Env{status: 200, body: body}}) do
+    {:ok, Parser.build(body)}
+  end
+end
---
+++ Chapter-4/Desafios/get_repo/test/get_repo/user_repo_test.exs
@@ -0,0 +1,78 @@
+defmodule GetRepo.Client.UserRepoTest do
+  use ExUnit.Case, async: true
+
+  import GetRepo.Factory
+
+
+  alias GetRepo.Client.{Parser, UserRepo}
+  alias Plug.Conn
+  alias GetRepo.Error
+
+  describe "get_user_repos/2" do
+    setup do
+      bypass = Bypass.open()
+
+      {:ok, bypass: bypass}
+    end
+
+    test "when username exist, returns the repos", %{bypass: bypass} do
+      username = "joaopealves"
+      body = build(:user_repos_body)
+      url = bypass_url(bypass.port)
+
+      Bypass.expect(bypass, "GET", "/users/#{username}/repos", fn conn ->
+        conn
+        |> Conn.put_resp_header("content-type", "application/json")
+        |> Conn.resp(200, body)
+      end)
+
+      response = UserRepo.get_user_repos(url, username)
+
+      expected_response = {
+        :ok,
+        [
+          %Parser{
+            description:
+              "Projeto desenvolvido durante a Next Level Week, feito pela @Rocketseat com 💜",
+            html_url: "https://github.com/joaopealves/inmana",
+            id: 359_424_756,
+            name: "inmana",
+            stargazers_count: 8
+          }
+        ]
+      }
+
+      assert expected_response == response
+    end
+
+    test "returns an error if user doesn't exist", %{bypass: bypass} do
+      username = "'"
+      url = bypass_url(bypass.port)
+
+      Bypass.expect(bypass, "GET", "/users/#{username}/repos", fn conn ->
+        conn
+        |> Conn.resp(404, "")
+      end)
+
+      response = UserRepo.get_user_repos(url, username)
+
+      expected_response = {:error, %Error{result: "Github username not found!", status: :not_found}}
+
+      assert expected_response == response
+    end
+
+    test "returns an error if server is down", %{bypass: bypass} do
+      username = "joaopealves"
+      url = bypass_url(bypass.port)
+
+      Bypass.down(bypass)
+
+      response = UserRepo.get_user_repos(url, username)
+
+      expected_response = {:error, %Error{result: :econnrefused, status: :bad_request}}
+      assert expected_response == response
+    end
+  end
+
+  defp bypass_url(port), do: "http://localhost:#{port}"
+end
